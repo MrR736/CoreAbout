d0140b619099f72e36094437db2e8747ed6f4b79
MrR736 <mrr6451@outlook.com>
2024-09-04 15:10:48 -0700

    Create Version.cmd

---
+++ Version.cmd
@@ -0,0 +1,31 @@
+@echo off
+set VERSION-CP=0.0.0.0.3
+set VERSION-CC=0.0.0.0.2
+set VERSION-CCo=0.0.0.0.2
+set VERSION-CSe=0.0.0.0.4
+set VERSION-CSo=0.0.0.0.2
+set VERSION-CU=0.0.0.0.4
+
+echo Core Prompt
+echo Version %VERSION-CP%
+echo:
+
+echo CoreCommands
+echo Version %VERSION-CCo%
+echo:
+
+echo CoreControls
+echo Version %VERSION-CC%
+echo:
+
+echo CoreSettings
+echo Version %VERSION-CSe%
+echo:
+
+echo CoreSotfwares
+echo Version %VERSION-CSo%
+echo:
+
+echo CoreUpdates
+echo Version %VERSION-CU%
+echo:
