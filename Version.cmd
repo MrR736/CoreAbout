65c43e05a9597877436d4b045855a86945b63a94
MrR736 <mrr6451@outlook.com>
2024-09-05 16:29:05 -0700

    Update Version.cmd

--- Version.cmd
+++ Version.cmd
@@ -2,8 +2,8 @@
 set VERSION-CP=0.0.0.0.3
 set VERSION-CC=0.0.0.0.4
 set VERSION-CCo=0.0.0.0.3
-set VERSION-CSe=0.0.0.0.4
-set VERSION-CSo=0.0.0.0.2
+set VERSION-CSe=0.0.0.0.5
+set VERSION-CSo=0.0.0.0.3
 set VERSION-CU=0.0.0.0.4
 
 echo Core Prompt
