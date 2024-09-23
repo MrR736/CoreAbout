573d748b4ee27ba5296da0773f0122ba5672ea07
MrR736 <mrr6451@outlook.com>
2024-09-23 16:50:54 -0700

    Update Source-Update.bat

--- Downloads/Source-Update.bat
+++ Downloads/Source-Update.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d %~dp0
-.\wget -q -O "Source-Bin.csp.xz" https://archive.org/download/source-bin-v-0.0.0.1.csp/Source-Bin-v0.0.0.1.csp.xz
+.\wget -q -O "Source-Bin.csp.xz" https://archive.org/download/source-bin-v-0.0.0.2.csp/source-bin-v-0.0.0.2.csp.xz
