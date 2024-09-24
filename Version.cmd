1964fbf7f6cdb8b2beb13cb2cb3334819365aa36
MrR736 <mrr6451@outlook.com>
2024-09-23 20:42:18 -0700

    Update Version.cmd

--- Version.cmd
+++ Version.cmd
@@ -2,7 +2,7 @@
 set VERSION-CP=0.0.0.0.4
 set VERSION-CC=0.0.0.0.5
 set VERSION-CCo=0.0.0.0.4
-set VERSION-CSe=0.0.0.0.5
+set VERSION-CSe=0.0.0.0.6
 set VERSION-CSo=0.0.0.0.4
 set VERSION-CU=0.0.0.0.7
 
