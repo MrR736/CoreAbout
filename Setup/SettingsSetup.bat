f51e1bfd728526147d67cee2f9be6bbb9e0e2411
MrR736 <mrr6451@outlook.com>
2024-09-26 17:16:13 -0700

    Update SettingsSetup.bat

--- Setup/SettingsSetup.bat
+++ Setup/SettingsSetup.bat
@@ -3,7 +3,6 @@ timeout /t 1 /nobreak > NUL
 setlocal enabledelayedexpansion
 set /p InstallPath=<InstallPath
 set "InstallPath=!InstallPath: =!"
-for /F "tokens=*" %i in ('dir /b "%InstallPath%\*.exe"') do taskkill /F /IM "%i"
 del /q %InstallPath%
 xcopy /s /y ".\Settings" "%InstallPath%"
 cd "%temp%"
