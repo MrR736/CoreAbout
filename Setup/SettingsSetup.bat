b14333f01455b045e8bae712c5eafa3e48282449
MrR736 <mrr6451@outlook.com>
2024-09-26 17:43:00 -0700

    Update SettingsSetup.bat

--- Setup/SettingsSetup.bat
+++ Setup/SettingsSetup.bat
@@ -4,7 +4,6 @@ timeout /t 3 /nobreak > NUL
 setlocal enabledelayedexpansion
 set /p InstallPath=<InstallPath
 set "InstallPath=!InstallPath:0 =!"
-del /q %InstallPath%
 xcopy /s /y ".\Settings" "%InstallPath%"
 cd "%temp%"
 start cmd /c rd /s /q "%temp%\Settings" & exit
