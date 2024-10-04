5ccd00b222a33c5d2b31da2946640a30df1d1feb
MrR736 <mrr6451@outlook.com>
2024-10-04 06:55:53 -0700

    Update Everything-d.cmd

--- Downloads/Everything-d.cmd
+++ Downloads/Everything-d.cmd
@@ -1,2 +1,2 @@
 @echo off
-%p%\wget.exe -q -O Everything.zip https://www.voidtools.com/Everything-1.4.1.1026.x64.zip
+"%p%\wget.exe" -q -O Everything.zip https://www.voidtools.com/Everything-1.4.1.1026.x64.zip
